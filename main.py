f2f4e75fe642a986e5608958a21f750a68cf28b4
jjp <jjp@jjp.com>
2017-08-12 10:40:47 -0500

    passed with unit test

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+import math
 import json
+
+class DummyGPS:
+    def GetLocation(self):
+        return (5,5);
+
+
 
 str = None
 with open('residential.json') as f:
     str = f.read();
 
 j = json.loads(str)
-pass
+
+gps = DummyGPS()
+#gps = SL4AGPS()
+
+radiusOfNotif = 5
+
+def CheckIfInsideCoordinate(dataX,dataY,gpsX,gpsY,radius):
+    d = math.sqrt( (dataX-gpsX)**2 + (dataY-gpsY)**2 )
+    if(radius<=d):
+        return (True,d)
+    return (False,d)
+
+
+b = CheckIfInsideCoordinate(5,5,10,10,4)
+assert(b[0]==True)
+b = CheckIfInsideCoordinate(5,5,10,10,10)
+assert(b[0]==False)
+
