443b2df16c821612ad1db6ece12a669cf8f618c8
jjp <jjp@jjp.com>
2017-08-12 12:28:23 -0500

    web script working

--- testWeb.py
+++ testWeb.py
@@ -2,6 +2,8 @@ import android
 droid = android.Android() 
 droid.webViewShow("/storage/emulated/0/sl4a/scripts/main.html")
 
-while True: 
-   result = droid.waitForEvent("say").result
-   droid.ttsSpeak(result["data"])
+while True:
+   result = droid.eventWaitFor('fetch_data').result
+   print  result
+#   droid.ttsSpeak(result["data"])
+
